b4fdb969a1717b6d7b3b476edd23bbe2ace6a106
Tim Evans <timevans666@gmail.com>
2026-07-20 14:25:07 +0100

    fix: guard service-worker runtime cache on res.ok/basic + waitUntil

--- public/sw.js
+++ public/sw.js
@@ -14,8 +14,10 @@ self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
     caches.match(e.request).then((hit) => hit || fetch(e.request).then((res) => {
-      const copy = res.clone();
-      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      if (res.ok && res.type === 'basic') {
+        const copy = res.clone();
+        e.waitUntil(caches.open(CACHE).then((c) => c.put(e.request, copy)));
+      }
       return res;
     }).catch(() => caches.match('/index.html')))
   );
